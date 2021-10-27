721b9ff8b46df46ce2a4fa53b1f69741807d90fa
weiwei <weiwei@example.com>
2021-10-27 18:01:09 +0800

    update: 优化写法

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 pip相关的包
 
 scorpiotool: excel的生成工具
+
+#上传方式
+pip install wheel # 安装wheel模块
+
+python setup.py sdist  # 源码包
+python setup.py bdist_wheel --universal # 打包为无需build的wheel。其中--universal表示py2和py3通用的pure python模块。不满足通用或pure条件的模块不需加此参数
+
+上传项目
+先在pypi注册一个账户：https://pypi.org/account/register/
+
+然后安装上传所需模块：
+
+pip install twine
+
+最后上传：
+
+twine upload dist/*
---
+++ build/lib/scorpiotool/operationXlsx.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import xlsxwriter
+
+
+def generate_excel(expenses, NAME):
+    """
+    自动顺序生成excel文件
+    :param expenses: 列表、数组，里面是字典形式key,value 例如：[{"name":'XXX'}]
+    :param NAME: new file name
+    :return: XXX.xlsx
+    """
+    workbook = xlsxwriter.Workbook('./{}.xlsx'.format(NAME))
+    worksheet = workbook.add_worksheet()
+    keysList = [k for k in expenses[0].keys()]
+    # 设定格式，等号左边格式名称自定义，字典中格式为指定选项
+    # bold：加粗，num_format:数字格式
+    bold_format = workbook.add_format({'bold': True})
+    # money_format = workbook.add_format({'num_format': '$#,##0'})
+    # date_format = workbook.add_format({'num_format': 'mmmm d yyyy'})
+    columnList = [chr(i) for i in range(ord("A"),ord("Z")+1)]
+    # 将二行二列设置宽度为15(从0开始)
+    worksheet.set_column(1, 1, 15)
+    for index, value in enumerate(keysList):
+        worksheet.write('%s1' % columnList[index], keysList[index], bold_format)
+    row = 1
+    col = 0
+    for index, item in enumerate(expenses):
+        # 使用write_string方法，指定数据格式写入数据
+        for i in range(len(expenses)):
+            if i >= len(list(item.keys())):
+                break
+            worksheet.write_string(row, col + i, str(item[list(item.keys())[col + i]]))
+        row += 1
+    workbook.close()
