3b25471837725b027c274c95222904c82a3cfe54
weiwei <weiwei@example.com>
2021-10-29 11:00:59 +0800

    update: 更新

--- setup.py
+++ setup.py
@@ -17,5 +17,5 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     platforms="any",
-    install_requires=["xlsxwriter"]
+    install_requires=["xlsxwriter", "hashlib", "redis", "gzip", "random"]
 )
